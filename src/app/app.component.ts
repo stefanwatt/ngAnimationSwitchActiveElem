73066879d9765e4e23a2cb5dc16394cd83df39ab
Stefan Watt <stefan.watt@gmail.com>
2022-04-24 23:36:50 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
 import { Component } from '@angular/core';
+import { AnimationEvent } from '@angular/animations';
+import {
+  slideInUpOnEnterAnimation,
+  slideOutDownOnLeaveAnimation,
+} from 'angular-animations';
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  query,
+  stagger,
+} from '@angular/animations';
+
+const listAnimationEnter = trigger('listAnimationEnter', [
+  transition('* <=> *', [
+    query(
+      ':enter',
+      [
+        style({ transform: 'translateY(100%)' }),
+        animate('600ms ease-in', style({ transform: 'translateY(0%)' })),
+      ],
+      { optional: true }
+    ),
+  ]),
+]);
+const listAnimationLeave = trigger('listAnimationLeave', [
+  transition('* <=> *', [
+    query(
+      ':leave',
+      [
+        style({ transform: 'translateY(0%)' }),
+        animate('600ms ease-in', style({ transform: 'translateY(100%)' })),
+      ],
+      { optional: true }
+    ),
+  ]),
+]);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    slideOutDownOnLeaveAnimation(),
+    slideInUpOnEnterAnimation(),
+    listAnimationEnter,
+    listAnimationLeave,
+  ],
 })
 export class AppComponent {
   title = 'animations';
+  elems = ['redElem', 'blueElem', 'greenElem'];
+  selectedElem = this.elems[0];
+  nextElem = this.elems[0];
+  message = '';
+
+  onSelectedElemChange = (e: AnimationEvent) => {
+    setTimeout(() => {
+      this.message = `That's enough. Come Back ${this.selectedElem}!`;
+      setTimeout(() => {
+        this.message = `It's your turn now ${this.nextElem}`;
+        setTimeout(() => {
+          this.selectedElem = this.nextElem;
+          this.message = '';
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [FormsModule, BrowserModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
